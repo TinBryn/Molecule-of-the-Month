450b85552d6d3ff77b07b70a72986ff3a371906c
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-09-08 13:16:57 +0800

    moved the html files into a folder for better organisation

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 
 app.set("/","html");
-app.use(express.static(path.join(__dirname,"/")));
+app.use(express.static(path.join(__dirname,"static")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-let port = parseInt(process.env.PORT || "8080") || 8080
+const port = parseInt(process.env.PORT || "8080") || 8080
 
 
 app.get('/',(req,res) => {
