5d41fad4c95856dc14d2793d9d83cdb7702ebf3c
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-09-23 17:31:00 +0800

    Restructured the web app to support multiple routes
    - The index is a now a landing page (and a very bad one)
    - The AR app is under the route "/molecule" this could be changed going forward
    - The CMS is under the route "/admin/cms" which is also subject to change
    
    I doubt I'm doing this the most idiomatic way, but I'm hoping I've separated things out so I can fix that up when I learn more about how express works.
    
    I'm particulary unsure about the "/admin/cms" route, I think there is a better way to have nested routes.

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 
 const express = require('express');
 const app = express();
-const path = require('path');
+const path = require("path");
 
-const enviroment = require('dotenv');
+const enviroment = require("dotenv");
 enviroment.config();
 
-app.set("/","html");
-app.use(express.static(path.join(__dirname,"static")));
+app.set("/", "html");
+app.use(express.static(path.join(__dirname, "static")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const port = parseInt(process.env.PORT || "8080") || 8080
 
-app.get('/',(req,res) => {
-    res.render('index');
-});
+configureRoutes(app);
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log("listen on http://localhost:" + port);
 });
+
+function configureRoutes(app) {
+    app.get('/', (_req, res) => {
+        res.sendFile(path.join(__dirname, "static", "index.html"));
+    });
+
+    app.get('/molecule', (req, res) => {
+        res.sendFile(path.join(__dirname, "static", "molecule.html"));
+    });
+
+    app.get("/admin/cms", (req, res) => {
+        res.sendFile(path.join(__dirname, "static", "cms.html"));
+    });
+}
