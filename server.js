87c03eeb2d9c4c7ea6cb25e0b38e602c13a59322
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-09-29 12:48:43 +0800

    Add new routes to create a landing page and others (#26)
    * Restructured the web app to support multiple routes
    
    - The index is a now a landing page (and a very bad one)
    - The AR app is under the route "/molecule" this could be changed going forward
    - The CMS is under the route "/admin/cms" which is also subject to change
    
    I doubt I'm doing this the most idiomatic way, but I'm hoping I've separated things out so I can fix that up when I learn more about how express works.
    
    I'm particulary unsure about the "/admin/cms" route, I think there is a better way to have nested routes.
    
    * added api for getting the molecule model
    
    * Moved some comments in the AR app
    
    - It seems that in commit 08555e768673cdb0a082a80d0ab4ddeec56d1108 where Songyuan added gesture controls, he moves the code around and in doing so separated some comments from what they are commenting on
    
    * Get the database Url from the deployment environment

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 
 const express = require('express');
 const app = express();
-const path = require('path');
+const path = require("path");
 
-const enviroment = require('dotenv');
+const enviroment = require("dotenv");
 enviroment.config();
 
-app.set("/","html");
-app.use(express.static(path.join(__dirname,"static")));
+app.set("/", "html");
+app.use(express.static(path.join(__dirname, "static")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const port = parseInt(process.env.PORT || "8080") || 8080
+// get the prot from the deployment environment or use 8080 as default
+const port = parseInt(process.env.PORT || "8080") || 8080;
 
-app.get('/',(req,res) => {
-    res.render('index');
-});
+// get the database url from the deployment environment
+const dbUrl = process.env.DATABASE_URL;
 
-app.listen(port,() => {
+configureRoutes(app);
+
+app.listen(port, () => {
     console.log("listen on http://localhost:" + port);
 });
+
+function configureRoutes(app) {
+    app.get('/', (_req, res) => {
+        res.sendFile(path.join(__dirname, "static", "index.html"));
+    });
+
+    app.get('/molecule', (req, res) => {
+        res.sendFile(path.join(__dirname, "static", "molecule.html"));
+    });
+
+    app.get("/admin/cms", (req, res) => {
+        res.sendFile(path.join(__dirname, "static", "cms.html"));
+    });
+
+    app.get("/api/molecule", (req, res) => {
+        res.sendFile(path.join(__dirname, "static", "molfile", "dna1.gltf"));
+    });
+}
