a76cb775cbc4155e9b1c3b170134e5e16d3cca79
Will <will@hatchet.com.au>
2021-10-03 19:05:54 +0800

    Added appropriate libraries for molecule to gltf conversion, some basic scene setup

--- convertor/moleculegltfconvertor.js
+++ convertor/moleculegltfconvertor.js
@@ -1,23 +1,57 @@
+const THREE = require('three');
+const Canvas = require('canvas');
+const exporter = require('three-gltf-exporter');
+const { Blob, FileReader } = require('vblob');
+
+// Patch global scope to imitate browser environment.
+// From https://gist.github.com/donmccurdy/9f094575c1f1a48a2ddda513898f6496
+global.window = global;
+global.Blob = Blob;
+global.FileReader = FileReader;
+global.THREE = THREE;
+global.document = {
+  createElement: (nodeName) => {
+    if (nodeName !== 'canvas') throw new Error(`Cannot create node ${nodeName}`);
+    const canvas = new Canvas(256, 256);
+    // This isn't working — currently need to avoid toBlob(), so export to embedded .gltf not .glb.
+    // canvas.toBlob = function () {
+    //   return new Blob([this.toBuffer()]);
+    // };
+    return canvas;
+  }
+};
 
 function getBallAndStick(moleculeObj) {
-    let nodeArray = new Array();
-    for(atom of moleculeObj.atoms) {
-        const atomElement = atom['element'];
-        const atomObj = {
-            name: atom['name'],
-            translation: [atom['x'], atom['y'], atom['z']]
-        };
-        nodeArray.push(atomObj);
-    }
 
-    console.log(nodeArray.length);
-    let gltfObj = {
-        asset: {version: "2.0"},
-        nodes: nodeArray,
-        scenes: [{name: "molecule", nodes: [0]}],
-        scene: 0,
-    };
-    console.log(JSON.stringify(gltfObj));
+    const loader = new exporter();
+    const scene = new THREE.Scene();
+    const fov = 75;
+
+    const camera = new THREE.PerspectiveCamera( 50, 0.5 * 2, 1, 10000 );
+    camera.position.z = 2500;
+
+    const baseSphere = new THREE.Sphere();
+    const baseMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+    for(atom of moleculeObj.atoms) {
+      const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+      const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+      const atomRender = new THREE.Mesh( geometry, material );
+      atomRender.x = atom['x'];
+      atomRender.y = atom['y'];
+      atomRender.z = atom['z'];
+      scene.add(atomRender);
+  }
+  
+
+    loader.parse(scene, (content) => {
+        fs = require('fs');
+        fs.writeFile('test.gltf', JSON.stringify(content), function (err) {
+          if (err) return console.log(err);
+        }); 
+      },);
+
+
 }
 
 
