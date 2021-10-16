d1749329121572e0d55f4345530baab99921c64b
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-10-16 11:46:40 +0800

    Renamed `templates/` to `views/` and `scripts/` to `js/`
    - also deleted empty js file that was not used

--- static/routes/site.js
+++ static/routes/site.js
@@ -3,21 +3,21 @@ const path = require("path");
 module.exports = app => {
     app.get("/", (_request, response) => {
         console.log("landing page");
-        response.sendFile(path.join(__dirname, "../templates/index.html"));
+        response.sendFile(path.join(__dirname, "../views/index.html"));
     })
     
     app.get("/molecule", (_request, response) => {
         console.log("app page");
-        response.sendFile(path.join(__dirname, "../templates/molecule.html"));
+        response.sendFile(path.join(__dirname, "../views/molecule.html"));
     })
     
     app.get("/markerless", (_request, res) => {
         console.log("experimental page");
-        response.sendFile(path.join(__dirname, "../templates/markerless.html"));
+        response.sendFile(path.join(__dirname, "../views/markerless.html"));
     })
     
     app.get("/admin/cms", (_request, res) => {
         console.log("CMS page");
-        response.sendFile(path.join(__dirname, "../templates/cms.html"));
+        response.sendFile(path.join(__dirname, "../views/cms.html"));
     });
 }
