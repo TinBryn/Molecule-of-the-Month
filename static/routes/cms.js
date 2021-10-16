5b4170d21cf07da87c85b5b768117ea9b0884400
danielgerardclaassen <danielgerardclaassen@gmail.com>
2021-10-16 21:47:30 +0800

    Finalizing the CMS application
    The changes in this commit include:
    1. Altering the storage of user details to be stored in the database
    2. Reconfigured passport config to match this usecase
    3. Altered all CMS views to make UI more readable
    4. Fixed and finalized the server file directory storage system. Users can now upload a file and specify the month they wish it to be active. Active files for the current month can then be read through the download route.
    5. This has a side effect of allowing files to be queued

--- static/routes/cms.js
+++ static/routes/cms.js
@@ -1,20 +1,43 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const fs = require('fs')
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 const initializePassport = require("../config/passport-config");
-const users = [];
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const {Client} = require('pg');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+//connections below are used in fileuploading
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
+app.use(methodOverride('_method'));
+
+client.connect();
+
 initializePassport(
     passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
+    email => client.query("SELECT * FROM users WHERE email='$1'", [email], (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            return JSON.stringify(res.rows[0])
+        }
+    }),
 );
-
 
 // These next two functions check respectively if the user is Authenticated/logged in or not.
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next()
     }
-
     res.redirect('/login')
 }
 
@@ -27,25 +50,26 @@ function checkNotAuthenticated(req, res, next) {
 
 module.exports = app => {
     // The below HTTP methods are used in the simple CMS app
-    // the /submit route is used to upload files. Only accessible by authorised users. 
+    // the /submit route is used to upload files. Only accessible by authorised users. The download route is responsible for serving the current month's file.
     app.get('/submit', checkAuthenticated, (req, res) => {
         res.render('submit.ejs', {
             name: req.user.name
         })
     })
-
+//The way the file structure queues files is by naming convention. Each file uploaded has a name corresponding to the month and year selected for it.
     app.post('/submit', checkAuthenticated, function (req, res) {
         let sampleFile;
         let uploadPath;
         if (!req.files || Object.keys(req.files).length === 0) {
             return res.status(400).send('No files were uploaded.');
         }
+        var d = String(req.body.time);
+        d = d.substring(0, d.length-3);
         sampleFile = req.files.sampleFile;
-        uploadPath = __dirname + '/files/' + sampleFile.name;
+        uploadPath = __dirname + '/files/' + d;
         sampleFile.mv(uploadPath, function (err) {
             if (err)
                 return res.status(500).send(err);
-
             res.send('File uploaded.');
         });
     });
@@ -54,30 +78,33 @@ module.exports = app => {
         res.render('login.ejs')
     })
 
-    app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    app.post('/login', (req,res,next) => {
+        checkNotAuthenticated;
+        next();
+        },
+        passport.authenticate('login', {
         successRedirect: '/submit',
         failureRedirect: '/login',
         failureFlash: true
-    }))
+    })
+    )
 
     app.get('/register', checkNotAuthenticated, (req, res) => {
         res.render('register.ejs')
     })
-
+//registered users have their details inserted into heroku database.
     app.post('/register', checkNotAuthenticated, async (req, res) => {
         try {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10)
-            users.push({
-                id: Date.now().toString(),
-                name: req.body.name,
-                email: req.body.email,
-                password: hashedPassword
-            })
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            client.query('INSERT INTO users(user_id,username,password,email) VALUES($1,$2,$3,$4)', [Date.now().toString(), req.body.name,hashedPassword,req.body.email], (err, res) => {
+                if (err) {
+                    console.log(err.stack)
+                }
+            });
             res.redirect('/login')
         } catch {
             res.redirect('/register')
         }
-        console.log(users)
     })
 
     // This route is used when users log out.
@@ -85,4 +112,35 @@ module.exports = app => {
         req.logOut()
         res.redirect('/login')
     })
+//This route will automatically serve the file corresponding to the current month and year only.
+    app.get('/download',function(req,res) {
+        var d = new Date();
+        var year = String(d.getFullYear())
+        var month = String(d.getMonth()+1)
+        var name = String(year + '-' + month)
+        const file = __dirname + '/files/' + name;
+        res.download(file);
+    })
 };
+
+const functions = {
+    addUser: (id,username,password,email) => client.query('INSERT INTO users(user_id,username,password,email) VALUES($1,$2,$3,$4)',[id,username,password,email]),
+    fileSubmit: () => {
+        fs.readFile(__dirname+'/test.txt', (error, data) => {
+            uploadPath = __dirname + '/TestDirectory/';
+            data.mv(uploadPath);
+        })
+    },
+    passwordEncrypt: (password) => {
+        var secret = bcrypt.hash(password,10);
+        return secret;
+    },
+    passwordDecrypt: (hashed, password) => {
+        bcrypt.compare(password, hashed, (err, valid) => {
+            if (valid) {
+                return true
+            }
+        })
+    }
+}
+module.exports = functions;
