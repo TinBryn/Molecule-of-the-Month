c21e541ab2d695e0c3605960142e9fae1efb7a24
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-10-16 13:04:32 +0800

    Cleaned up the server.js a little

--- server.js
+++ server.js
@@ -3,32 +3,23 @@ enviroment.config();
 
 // get the port from the deployment environment or use 8080 as default
 const port = normalizePort(process.env.PORT || "8080");
-
-const app = require("./app");
-
-app.set("port", port);
-
 // get the database url from the deployment environment
 const dbUrl = process.env.DATABASE_URL;
 
-configureRoutes(app);
+
+const app = require("./app");
+app.set("port", port);
+
+const siteRouter = require("./static/routes/site");
+const apiRouter = require("./static/routes/api");
+const cmsRouter = require("./static/routes/cms");
+siteRouter(app);
+apiRouter(app);
+cmsRouter(app);
 
 app.listen(port, () => {
     console.log("listen on http://localhost:" + port);
 });
-
-function configureRoutes(app) {
-
-    const siteRouter = require("./static/routes/site");
-    siteRouter(app);
-
-    const apiRouter = require("./static/routes/api");
-    apiRouter(app);
-
-    const cmsRouter = require("./static/routes/cms");
-    cmsRouter(app);
-}
-
 
 /**
  ** Normalize a port into a number, string, or false.
